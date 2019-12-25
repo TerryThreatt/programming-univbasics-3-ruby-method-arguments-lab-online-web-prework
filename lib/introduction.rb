c94663d229f88fa73e28409736c0d99f7cd113ac
Terry Threatt <terry.threatt@gmail.com>
2019-12-25 00:12:52 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -18,7 +18,10 @@ end
 introduction_with_language("Terry", "Ruby")
 
 def introduction_with_language_optional(name, language = "Ruby")
-  puts "H"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language_optional("James")
 
 
 
